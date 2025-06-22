022fa93cc32f352d3c3aaf413ac4af4acffd6a13
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-06-22 14:30:11 +0300

    Add project files.

---
+++ FirstSheet/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Deniz Baloğlu");
+Console.WriteLine("Kariyerime yeni bir yol yaratmak istiyorum, seçeneklerimi görebilmek adına da ilk olarak backend developer olarak başlamak istedim.");
