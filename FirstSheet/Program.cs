38352c1ebfb5279b0c1bcd8940b076d5da88c3f1
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-06-22 15:20:27 +0300

    Done

--- FirstSheet/Program.cs
+++ FirstSheet/Program.cs
@@ -1,2 +1,2 @@
 ﻿Console.WriteLine("Deniz Baloğlu");
-Console.WriteLine("Backend developer kursuna katılmamadaki amacım yeni kod becerisi kazanarak bunu uygulamalar yaratarak kullanmak istiyorum.");
+Console.WriteLine("Backend developer kursuna katılmamdaki amacım yeni kod becerisi kazanarak bunu uygulamalar yaratarak kullanmak istiyorum.");
