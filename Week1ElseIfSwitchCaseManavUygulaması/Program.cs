007de36a926b75ecddacf61c202b1c2aee67eddd
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-06-23 00:40:20 +0300

    Manav Uygulaması Switch yöntemi ile yüklenmiştir.

---
+++ Week1ElseIfSwitchCaseManavUygulaması/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Rüya Manavına Hoşgeldiniz!"); // Print welcome message to the user
+Console.Write("\nElma = 2 TL"); // Print the price of apple
+Console.Write("\nArmut = 3 TL"); // Print the price of pear
+Console.Write("\nMuz = 4 TL"); // Print the price of banana
+Console.Write("\nDiger bütün meyveler = 4 TL"); // Print the price of all other fruits as default price
+
+Console.Write("\nLütfen almak istediğiniz meyvenin adını giriniz: "); // Prompt the user to enter the name of the fruit they want to buy
+switch (Console.ReadLine().ToLower()) // Read user input and convert it to lowercase for case-insensitive comparison
+{
+    case "elma": // Check if the input is "elma"
+        Console.WriteLine("Seçtiğiniz meyvenin fiyatı: 2 TL"); // Print the price of apple  
+        break;
+    case "armut":
+        Console.WriteLine("Seçtiğiniz meyvenin fiyatı: 3 TL"); // Print the price of pear
+        break;
+    case "muz":
+        Console.WriteLine("Seçtiğiniz meyvenin fiyatı: 4 TL"); // Print the price of banana
+        break;
+    default:
+        Console.WriteLine("Seçtiğiniz meyvenin fiyatı: 4 TL"); // Print the default price for all other fruits
+        break;
+}
--- Week1IfElsePratik/Program.cs
+++ Week1IfElsePratik/Program.cs
@@ -9,6 +9,7 @@ else if ( input == 10) // check if input is equal to 10
 {
     Console.WriteLine("Sayı 10!a eşittir."); // if it is equal to 10, print this message
 }
+
   if (input %2 == 0) // check if the input is even by using modulus operator
     Console.WriteLine("Sayı çift sayıdır."); // if condition is true, print this message
 else if ( input %2 != 0) // check if the input is odd by using modulus operator
