81f5c91098518bdb835f5aa0d46191bb84d657ea
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-06-22 23:58:35 +0300

    Cohort yüklenme durumu hazır.

---
+++ Week1IfElsePratik/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Lütfen bir sayı giriniz: ");
+int input = Convert.ToInt32(Console.ReadLine());
+if (input > 10)
+{ Console.WriteLine("Sayı 10'dan büyüktür."); }
+else if ( input < 10 )
+{ Console.WriteLine("Sayı 10'dan küçüktür."); }
+else if ( input == 10 )
+{
+    Console.WriteLine("Sayı 10!a eşittir.");
+}
+  if (input %2 == 0)
+    Console.WriteLine("Sayı çift sayıdır.");
+else if ( input %2 != 0)
+{ Console.WriteLine("Sayı tek sayıdır."); }
